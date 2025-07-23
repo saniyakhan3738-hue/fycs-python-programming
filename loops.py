574efb490e2732108c529b06525368bb954f6b78
saniyakhan3738-hue <saniyakhan3738@gmail.com>
2025-07-23 15:59:29 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,9 @@
+while True :
+    name = input("enter your int((Type exit to stop):")
+    if name == "exit":
+        break
+    print('hello',int)
+
+
+
+    
---
+++ marks.py
@@ -0,0 +1,28 @@
+Python 3.12.10 (tags/v3.12.10:0cc8128, Apr  8 2025, 12:21:36) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+========== RESTART: D:/SANIYA KHAN/python.py =========
+enter your marks:65
+Traceback (most recent call last):
+  File "D:/SANIYA KHAN/python.py", line 2, in <module>
+    if mark>= 101:
+NameError: name 'mark' is not defined. Did you mean: 'marks'?
+>>> 
+>>> 
+========== RESTART: D:/SANIYA KHAN/python.py =========
+enter your marks:75
+Traceback (most recent call last):
+  File "D:/SANIYA KHAN/python.py", line 2, in <module>
+    if mark>= 101:
+NameError: name 'mark' is not defined. Did you mean: 'marks'?
+>>> 
+========== RESTART: D:/SANIYA KHAN/python.py =========
+enter your marks:100
+Traceback (most recent call last):
+  File "D:/SANIYA KHAN/python.py", line 2, in <module>
+    if mark>= 100:
+NameError: name 'mark' is not defined. Did you mean: 'marks'?
+>>> 
+========== RESTART: D:/SANIYA KHAN/python.py =========
+enter your marks:65
+Grade C
