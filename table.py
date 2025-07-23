b23579bf62ce3bf6def0400a76ac3ea85f018e29
saniyakhan3738-hue <saniyakhan3738@gmail.com>
2025-07-23 16:27:38 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,27 @@
+Python 3.12.10 (tags/v3.12.10:0cc8128, Apr  8 2025, 12:21:36) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+========== RESTART: D:/SANIYA KHAN/python.py =========
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+>>> 
+========== RESTART: D:/SANIYA KHAN/python.py =========
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+HELLO WORLD
